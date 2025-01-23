5f108de5fcdd3d63a371bc1ec6b514973e34cc15
Minzoo1 <lollibop@nate.com>
2025-01-23 18:22:43 +0900

    lastver

--- 1231/vite.config.js
+++ 1231/vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  build: {
+    outDir: "dist", // Vercel에서 사용하는 출력 디렉토리
+  },
 
   resolve: {
     alias: {
